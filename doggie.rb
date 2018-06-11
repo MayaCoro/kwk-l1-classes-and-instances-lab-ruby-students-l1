9fa19c9c4d6480427f0161bad2bd95901ba2d18a
MayaCoro <mayascoro@gmail.com>
2018-06-11 16:03:15 +0000

    automatically backed up by learn

--- doggie.rb
+++ doggie.rb
@@ -1,2 +1,24 @@
 class Dog 
-  
+   def name=(dog_name)
+    @this_dog_name = dog_name
+  end
+  def name
+    @this_dog_name
+  end
+end
+   
+   def breed=(dog_breed)
+    @this_dog_breed = dog_breed
+  end
+  def breed
+    @this_dog_breed
+  end
+end
+
+fido = Dog.new
+fido.name = "Fido"
+puts "The dog's name is #{fido.name}."
+
+fido = Dog.new
+fido.name = "Fido"
+puts "The dog's name is #{fido.name}."
