9097f3d553ee9ec8c0f745cb94880a382afa1d87
MayaCoro <mayascoro@gmail.com>
2018-06-11 15:58:57 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,21 @@
 # dog.rb
+class Dog 
+  def name=(dog_name)
+    @this_dog_name = dog_name
+  end
+  def name
+    @this_dog_name
+  end
+end
+
+fido = Dog.new
+fido.name = "Fido"
+puts "The dog's name is #{fido.name}."
+
+snoopy = Dog.new
+snoopy.name = "Snoopy"
+puts "The dog's name is #{snoopy.name}."
+
+lassie = Dog.new
+lassie.name = "Lassie"
+puts "The dog's name is #{lassie.name}."
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,17 @@
 # person.rb
+class Person
+  def name=(person_name)
+    @this_persons_name = person_name
+  end
+  def name
+    @this_persons_name
+  end
+end
+
+adele_goldberg = Person.new
+adele_goldberg.name = "Adele Goldberg"
+puts "The person's name is #{adele_goldberg.name}."
+
+alan_kay = Person.new
+alan_kay.name = "Alan Kay"
+puts "The person's name is #{alan_kay.name}."
